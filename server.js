99552404f6df74b20b80004569dce8b7f320152b
Marciokkkjjj <148603386+Marciokkkjjj@users.noreply.github.com>
2024-10-11 22:57:17 -0300

    aa

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ io.on('connection', socket => {
     });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 443;
 server.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
